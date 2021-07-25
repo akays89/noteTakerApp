92b4cb14ecc4f2292964450ddd1141b350b4b86c
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-25 02:29:16 -0500

    created html and api routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Dependencies
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -7,6 +8,8 @@ const fs = require("fs");
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
 
 
         
@@ -19,4 +22,26 @@ app.use(express.static("public"))
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
+});
+
+// html routes
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+//   api routes
+
+app.get('/api/notes', function (req, res) {
+  return res.json(savedNotes());
+});
+
+app.post('/api/notes', function (req, res) {
+    let newNote = req.body;
+    updatedNotes(newNote);
+    return res.json(newNote);
+});
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
